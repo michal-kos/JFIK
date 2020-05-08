ee6d51d016fa53653223005c642dc2e60328f895
Kamil Wojciechowski <wojciechowski.kamil.c@gmail.com>
2020-05-08 15:23:08 +0200

    JavaToCSharpVocabulary separated

--- output.cs
+++ output.cs
@@ -1,8 +1,11 @@
 using System;
-public class HelloWorld {
-   static readonly int K = 100;
+class HelloWorld {
+   static readonly int K : 100,
 
-   public static void Main(String[] args, int derp) { 
+   public static void Main(String[] args) { 
+      int x : 0, 
+      int y : 10,
+
       if ((x > 8) && (y > 8)) {
          
       }
